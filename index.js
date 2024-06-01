20a45d0cd3df009183377431e16f8f5df1f31631
Ikenna Akano <95446875+ikennaex@users.noreply.github.com>
2024-06-01 20:36:23 +0100

    update index

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: "https://depathway360.org/", 
+    origin: "https://depathway360.org", 
     // was localhost 
   })
 );
